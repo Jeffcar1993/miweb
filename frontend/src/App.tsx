893f3935a4889e76d753ea6b12c5077740599e43
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-03-20 14:43:49 -0500

    feat: add Home page and update routing in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './pages/Home'
 
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
         <Header />
         <main className="flex-1">
           <Routes>
-            {/* <Route path="/" element={<Home />} />
-            <Route path="/proyectos" element={<About />} />
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/proyectos" element={<About />} />
             <Route path="/noticias" element={<Projects />} />
             <Route path="/en-vivo" element={<News />} />
             <Route path="/contactame" element={<News />} /> */}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+      <section className="text-center py-20">
+        <h2 className="text-4xl font-bold">¡Bienvenido a mi portafolio!</h2>
+        <p className="text-lg mt-4">¡Bienvenido a bordo! 🚀
+            Soy un Desarrollador Full Stack, apasionado por crear soluciones digitales eficientes y atractivas. Aquí encontrarás proyectos, ideas y servicios en frontend y backend, optimizados para ofrecer la mejor experiencia de usuario.
+            💡 Transformo ideas en código y código en soluciones innovadoras.
+            💻 Tecnologías modernas, desarrollo ágil y enfoque en calidad.
+            Si buscas a alguien para llevar tu proyecto al siguiente nivel, ¡hablemos! 💬</p>
+      </section>
+    );
+  }
+  
+  export default Home;
+  
