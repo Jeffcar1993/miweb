1c55ffc1403f0df42f9727530d0fcb886b605b8a
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-03-20 14:31:46 -0500

    fix: correct import case for Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 
